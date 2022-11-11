ae537b89f5dd40800269f833070207d17a86251a
frea45 <110092585+frea45@users.noreply.github.com>
2022-11-11 10:51:27 +0330

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,42 +1,39 @@
-# (c) @AbirHasan2005
-
-# Don't Forget That I Made This!
-# So Give Credits!
-
+# (c) @FarshidBand
 
 import os
 
 
 class Config(object):
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	API_ID = int(os.environ.get("API_ID", 12345))
-	API_HASH = os.environ.get("API_HASH")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN","5088657122:AAFwp9oaTQh92NWMXZRkV6DUCywpmBM3B5E")
+	API_ID = int(os.environ.get("API_ID", "3335796"))
+	API_HASH = os.environ.get("API_HASH", "138b992a0e672e8346d8439c3f42ea78")
 	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "NoNeed")
 	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "NoNeed")
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001482606933"))
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001482606933")
 	DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
 	PRESET = os.environ.get("PRESET", "ultrafast")
-	OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
-	CAPTION = "By @AHToolsBot"
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "VideoWatermark_Bot")
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	OWNER_ID = int(os.environ.get("OWNER_ID", "763990585"))
+	CAPTION = "★ @FarshidBand ™"
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "fi2li123robot")
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://abirhasan2005:abirhasan@cluster0.lb2tp.mongodb.net/cluster0?retryWrites=true&w=majority")
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-	ALLOW_UPLOAD_TO_STREAMTAPE = bool(os.environ.get("ALLOW_UPLOAD_TO_STREAMTAPE", False))
+	ALLOW_UPLOAD_TO_STREAMTAPE = bool(os.environ.get("ALLOW_UPLOAD_TO_STREAMTAPE", True))
 	USAGE_WATERMARK_ADDER = """
-Hi, I am Video Watermark Adder Bot!
+**👋 سلام دوست عزیز خوش آمدید ✨**
 
-**How to Added Watermark to a Video?**
-**Usage:** First Send a JPG Image/Logo, then send any Video. Better add watermark to a MP4 or MKV Video.
+**🔘 من ربات کاربردی واترمارک فایل های ویدیویی هستم.** 
 
-__Note: I can only process one video at a time. As my server is Heroku, my health is not good. If you have any issues with Adding Watermark to a Video, then please Report at [Support Group](https://t.me/linux_repo).__
+**✅ پشتیبانی از حداکثر حجم 2GB** 
 
-Desgined by @AbirHasan2005
+**🎌 فعالیت ربات ← /status **
+‌**⚙️⁩ تنظیمات واترمارک ← /settings** 
+
+**👤 مدیر : [FāRSHíD-Band](https://t.me/FarshidBand)**
 """
 	PROGRESS = """
-Percentage : {0}%
-Done ✅: {1}
-Total 🌀: {2}
-Speed 🚀: {3}/s
-ETA 🕰: {4}
+• ✅{1} of 📁{2} •
+📊 **درصد دانلود شده** : {0}%
+🚀 **سرعت** : {3}/s
+⁦⌚⁩ **مدت زمان اتمام پروژه** : {4}
 """
